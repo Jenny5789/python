96408e917e8aeced787f2c290f9513bc19fb4a4b
EJaeChoi <ejae2195@naver.com>
2026-03-09 13:08:09 +0900

    tkinter 260309.py
    https://wikidocs.net/295105
    
    연습

--- tkinker260309.py
+++ tkinker260309.py
@@ -33,3 +33,37 @@ root.mainloop()
 
 ### 2. 텍스트 요소들 - 정보를 보여주고 받기###
 ##Label - 텍스트와 이미지 표시##
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Lable 연습")
+root.geometry("500x400")
+root.configure(bg= "white")
+
+## 기본 라벨
+basic_label = tk.Label(root, text = "안녕하세요! 이것은 기본 라벨입니다.")
+basic_label.pack(pady=10)
+
+#스타일이 적용된 라벨
+styled_label = tk.Label(
+    root,
+    text="예쁘게 꾸민 라벨",
+    font=("맑은고딕", 16, "bold"),   #폰트 설정
+    fg="blue",                       #글자색
+    bg="lightyellow",                #배경색
+    width=20,                        #너비(글자 수)
+    height=2                         #높이(줄 수)
+)
+styled_label.pack(pady=10)
+
+# 여러 줄 라벨
+multiline_label = tk.Label(
+    root,
+    text="여러 줄로 된 라벨입니다. \n두 번째 줄\n세 번째 줄",
+    font=("맑은고딕", 12),
+    justify=tk.LEFT,                   #텍스트 정렬
+    bg="lightgreen"
+)
+multiline_label.pack(pady=10)
+
+root.mainloop()
